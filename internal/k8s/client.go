4bd2fe37b1798e2cdfbb901a7a8e3c598bfd2fde
EasyLab Dev <dev@easylab.local>
2026-09-16 17:53:52 +0000

    chart: egress sidecar v0.5.3 (h2 ALPN + hex.pm/api.hex.pm routing)

--- internal/k8s/client.go
+++ internal/k8s/client.go
@@ -242,6 +242,7 @@ var defaultUpstreams = []egressDomain{
 	{Match: []string{"rubygems.org", "index.rubygems.org"}},
 	{Match: []string{"repo.packagist.org"}},
 	{Match: []string{"repo.hex.pm"}},
+	{Match: []string{"hex.pm", "api.hex.pm"}},
 	{Match: []string{"pub.dev"}},
 	{Match: []string{"charts.helm.sh"}},
 	{Match: []string{"center.conan.io", "center2.conan.io"}},
